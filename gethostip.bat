3bca82df3a241b495121413081119bfdd9d603fe
Vince Nándorfi <vince.nandorfi@affinitext.com>
2023-03-08 18:03:24 +0100

    removed tmp files

--- gethostip.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-for /f "tokens=1" %%i in ('hostname') do set host=%%i
-for /f "tokens=1" %%i in ('ping -4 %host% -n 1 ^| findstr /i "Pinging"') do set ip=%%i
-set ip=%ip:~8%
-setx MY_IP %ip%
-echo A gép IP címe: %ip%
-echo A környezeti változó: %MY_IP%
