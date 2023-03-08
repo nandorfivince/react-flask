f74b135618f7679c236e068e9ededa776d752f98
Vince Nándorfi <vince.nandorfi@affinitext.com>
2023-03-08 17:24:32 +0100

    sulyemeles init frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [text, setText] = useState("");
-  const [consoleText, setConsoleText] = useState("");
-
-  const handleTextChange = (event) => {
-    setText(event.target.value);
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetch('/console', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/plain',
-        'Accept': 'text/plain'
-      },
-      body: text
-    })
-    .then(response => response.text())
-    .then(data => {
-      console.log(data);
-      setConsoleText(data);
-    })
-    .catch(error => console.log(error))
-  }
-
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      handleSubmit(event);
-    }
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Enter some text:
-        </p>
-        <input type="text" value={text} onChange={handleTextChange} onKeyPress={handleKeyPress} />
-        <br />
-        <button onClick={handleSubmit}>Submit</button>
-        <br />
-        <p>
-          Console Text:
-        </p>
-        <textarea value={consoleText} readOnly />
-      </header>
+      <div className="header">
+        <h1 className="title">Súlyemelő verseny</h1>
+        <h3 className="date">Budapest - 2023.03.08 - 17:07</h3>
+      </div>
+      <div className="container">
+        <div className="column">
+          <h2 className="subtitle">Felkészül</h2>
+        </div>
+        <div className="separator"></div>
+        <div className="column">
+          <h2 className="subtitle">Gyakorlatot végez</h2>
+          <p>Hamzsabégi Marika</p>
+        </div>
+        <div className="separator"></div>
+        <div className="column">
+          <h2 className="subtitle">Információk</h2>
+          <p>Nő</p>
+          <p>70 kg</p>
+          <p>165 cm</p>
+          <p>54 éves</p>
+        </div>
+      </div>
     </div>
   );
 }
